baaa9022437bc3c2c3860f5dba3cf2521eb82bb3
High23 <warenasb2@gmail.com>
2024-06-25 01:52:19 -0700

    Homepage now shows posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import DisplayPosts from './components/post/post'
 
-function App() {
-    
-    const siteURL = 'https://tourmaline-amenable-quotation.glitch.me/'
+export default function App() {
+    const [posts, setPosts] = useState(null)
+    const [err, setErr] = useState(null)
+    useEffect(() => {
+        async function fetchPosts() {
+            const data = await fetch('https://tourmaline-amenable-quotation.glitch.me/')
+                .then(response => response.json())
+                .catch(error => {
+                    setErr(error)
+                });
+            setPosts(data.posts)
+        }
+        fetchPosts()
+        return () => {
+            setPosts(null)
+        }
+    }, [])
     return (
         <div>
-            <header>Blog Posts</header>
+            <header>posts</header>
+            {(err === null && posts !== null) && <DisplayPosts posts={posts}></DisplayPosts>}
         </div>
     )
 }
-
-export default App
