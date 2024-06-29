2c87e6ece286d09e83746ac66050896a98de539b
High23 <warenasb2@gmail.com>
2024-06-28 23:36:17 -0700

    Navbar will change content if the token exists

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
 
 export default function NavBar({token}) {
+    //const token = localStorage.getItem('token')
     return (
         <header>
             <Link to='/'>Home</Link>
-            <Link to='/login'>Log In</Link>
-            <Link to='/sign-up'>Sign Up</Link>
+            { token === null ? 
+            <>
+                <Link to='/login'>Log In</Link>
+                <Link to='/sign-up'>Sign Up</Link>
+            </> :
+            <>
+                <Link to='/logout'>Log Out</Link>
+                <Link to='/user'>Profile</Link>
+            </>
+            }
         </header>
     )
 }
 
+// <Link to='/logout'>Log Out</Link> Enter this later when logout has been implemented
