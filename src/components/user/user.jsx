0f1fbc39809d5b64e47d848d096628601f5c8514
High23 <warenasb2@gmail.com>
2024-07-01 02:38:51 -0700

    Users can now edit their name, status and password

--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -28,13 +28,13 @@ export function CurrentUser() {
     // maybe use params here instead of outlet
     return (
         <>
-            <NavBar></NavBar>
-            { (currentUser !== null && params.params === undefined) && 
+            <NavBar token={token}></NavBar>
+            { (currentUser !== null) && 
             <div> 
                 <h3>Username: { currentUser.username }</h3>
                 <div>Author status: { currentUser.author === false ? "Not an author" : "Is an author" }</div>
                 <button type="button" onClick={() => {
-                    navigate("edit")
+                    navigate(`${currentUser._id}/edit`)
                 }}>Edit user </button>
             </div>
             }
@@ -43,19 +43,65 @@ export function CurrentUser() {
 }
 
 export function EditCurrentUser() {
-    
+    const [errors, setErrors] = useState(null);
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+    const params = useParams()
+    const siteUrl = `https://tourmaline-amenable-quotation.glitch.me/user/${params.userId}/edit`;
+
+    async function formSubmission(form) {
+        form.preventDefault();
+        const formData = new FormData(form.target);
+        const data = `username=${formData.get('username')}&password=${formData.get('password')}&author=${formData.get('authorCheckBox')}`;
+        const response = await fetch(siteUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Bearer " + token
+            },
+            body: data,
+        });
+        
+        const info = await response.json();
+        console.log(info)
+        if (info.errors) {
+            setErrors(info.errors);
+        } else {
+            localStorage.setItem('token', info.token);
+            navigate("/user", {replace: true});
+        }
+    }
+
     return (
         <>
-            <NavBar></NavBar>
+            <NavBar token={token}></NavBar>
+            <form action="" method='post' onSubmit={(form) => {formSubmission(form)}}>
+                <div>
+                    <label htmlFor="username">New username: </label>
+                    {errors === 'Username does not exist.' && <div>{errors}</div>}
+                    <input type="text" name="username" id="username"/>
+                </div>
+                <div>
+                    <label htmlFor="password">New password: </label>
+                    {errors === 'Invalid password' && <div>{errors}</div>}
+                    <input type="password" name="password" id="password" autoComplete='off' />
+                </div>
+                <div>
+                    <label htmlFor="authorCheckBox">Author: </label>
+                    {errors === 'Invalid password' && <div>{errors}</div>}
+                    <input type="checkbox" name="authorCheckBox" id="authorCheckBox"/>
+                </div>
+                <button>Save Changes</button>
+            </form>
         </>
     )
 }
 
 export function User() {
-    
+    const token = localStorage.getItem('token');
     return (
         <>
-            <NavBar></NavBar>
+            <NavBar token={token}></NavBar>
             <p>Random user info</p>
         </>
     )
