52618f1d820488b7765d1b60080aede530440816
High23 <warenasb2@gmail.com>
2024-07-05 00:29:11 -0700

    Authors can now see a create post button

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,21 +1,52 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom'
 import './navbar.css'
 
 export default function NavBar({token}) {
-    return (
-        <header>
-            <Link to='/'>Home</Link>
-            { (token === null || token === undefined) ? 
-            <>
-                <Link to='/login'>Log In</Link>
-                <Link to='/sign-up'>Sign Up</Link>
-            </> :
-            <>
-                <Link to='/logout'>Log Out</Link>
-                <Link to='/user'>Profile</Link>
-            </>
+    const [currentUser, setCurrentUser] = useState(null)
+    const [errors, setErrors] = useState(undefined)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        async function fetchCurrentUser() {
+            const response = await fetch(import.meta.env.VITE_SITEURL + 'user', {
+                headers: {
+                    "Authorization": "Bearer " + token
+                }
+            });
+            const data = await response.json()
+            if (response.status !== 200) {
+                setErrors("Invalid token!")
+                navigate('/logout', {replace: true});
+                return;
             }
-        </header>
+            console.log(data)
+            setCurrentUser(data.user)
+        }
+
+        if (token) {
+            fetchCurrentUser()
+        }
+    }, []);
+
+    return (
+        <>
+            <header>
+                <Link to='/'>Home</Link>
+                { (token === null || token === undefined) ? 
+                <>
+                    <Link to='/login'>Log In</Link>
+                    <Link to='/sign-up'>Sign Up</Link>
+                </> :
+                <>
+                    {((errors === undefined && currentUser !== null) && currentUser.author) && <Link to='/post/create'>Create a Post</Link>}
+                    <Link to='/logout'>Log Out</Link>
+                    <Link to='/user'>Profile</Link>
+                </>
+                }
+            </header>
+        </>
+        
     )
 }
