dedf5402717c9b2e18f75181e2fc960f0974a1b1
High23 <warenasb2@gmail.com>
2024-07-06 01:04:54 -0700

    Created form component to create posts

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -123,19 +123,94 @@ function Post() {
 
 function CreatePost() {
     const token = localStorage.getItem('token')
+    const [error, setError] = useState(null)
+    useEffect(() => {
+        if (token === null) {
+            setError("You do not have a token!")
+            return
+        }
+        async function verifyAuthorStatus() {
+            const response = await fetch(import.meta.env.VITE_SITEURL + "post/create", {
+                headers: {
+                    "Authorization": "Bearer " + token
+                }
+            });
+            if (response.status === 500) {
+                setError("An error has occurred fetching this page. You either do not have access or a valid token!")
+            }
+        }
+        verifyAuthorStatus()
+    }, [])
+
     return (
         <>
             <NavBar token={token}></NavBar>
-            <CreateForm></CreateForm>
+            {error === null ? 
+            <CreateForm token={token}></CreateForm>
+            : <>{error}</>}
         </>
     )
 }
 
-function CreateForm() {
+function CreateForm({token}) {
+    const [errors, setErrors] = useState(null);
+    const navigate = useNavigate()
+
+    async function formSubmission(form) {
+        form.preventDefault();
+        const formData = new FormData(form.target);
+        const data = `title=${formData.get('postTitle')}&text=${formData.get('postText')}&published=${formData.get('publishCheckBox')}`;
+        const response = await fetch(import.meta.env.VITE_SITEURL + "post/create", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Bearer " + token,
+            },
+            body: data,
+        });
+        const info = await response.json();
+        if (info.errors) {
+            setErrors(info.errors);
+            return
+        }
+        navigate(`/post/${info.postId}`, {replace: true})
+    }
+
     return (
-        <form>
-            input
-        </form>
+        <>
+            <form action="" method='post' onSubmit={(form) => {formSubmission(form)}}>
+                <div>
+                    <label htmlFor="postTitle">Title: </label>
+                    {}
+                    <input type="text" name="postTitle" id="postTitle" autoComplete='off' />
+                </div>
+                <div>
+                    <label htmlFor="postText">Text: </label>
+                    <textarea type="text" name="postText" id="postText" cols={120} rows={30}  />
+                </div>
+                <div>
+                    <label htmlFor="publishCheckBox">Publish: </label>
+                    <input type="checkbox" name="publishCheckBox" id="publishCheckBox"/>
+                </div>
+                <button>Create post</button>
+            </form>
+            {errors !== null && <PostCreationErrors errors={errors}></PostCreationErrors>}
+        </>
+    )
+}
+
+
+function PostCreationErrors({errors}) {
+    return (
+        <div> Post creation error(s):
+            {errors.map((err, index) => {
+                return (
+                    <div key={index}>
+                        <span>{err.msg}</span>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
