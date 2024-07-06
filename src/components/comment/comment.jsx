9c45dd08e4f3e1e0f69b78260946a6b0b5b71b81
High23 <warenasb2@gmail.com>
2024-07-06 01:06:33 -0700

    Made/moved comment form to proper component/file

--- src/components/comment/comment.jsx
+++ src/components/comment/comment.jsx
@@ -1,8 +1,29 @@
 import { useState } from 'react'
-import './comment.css'
+import { useNavigate } from 'react-router-dom'
 
-function Comment() {
-    
+export default function CommentForm({data, formSubmission}) {
+    const navigate = useNavigate()
+    return (
+        <section>
+            <h2>Comments</h2>
+            <form action="" method="post" onSubmit={(form) => {formSubmission(form)}}>
+                <label htmlFor="commentText"></label>
+                <textarea type="text" name="commentText" id="commentText" cols={40} rows={3} min={2} max={250} required onInvalid={(e) => {
+                    e.target.setCustomValidity("A comment must be between 2 and 250 characters long!")
+                }}/>
+                <button>Comment</button>
+            </form>
+        { data.comments.length > 0 && data.comments.map((comment) => {
+            return (
+                <div key={comment._id}>
+                    <h4 className='username clickable' onClick={() => {
+                            navigate(`/user/${comment.author._id}`);
+                    }}>{comment.author.username}</h4>
+                    <div>{comment.date}</div>
+                    <p>{comment.text}</p>
+                </div>
+            )
+        })}
+        </section>
+    )
 }
-
-export default Comment
--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
+import CommentForm from '../comment/comment';
 import NavBar from '../navbar/navbar';
 import './post.css'
 
@@ -91,27 +92,7 @@ function Post() {
                         <div>{data.post.date}</div>
                         <p>{data.post.text}</p>
                     </section>
-                    <section>
-                        <h2>Comments</h2>
-                        <form action="" method="post" onSubmit={(form) => {formSubmission(form)}}>
-                            <label htmlFor="commentText"></label>
-                            <textarea type="text" name="commentText" id="commentText" cols={40} rows={3} min={2} max={250} required onInvalid={(e) => {
-                                e.target.setCustomValidity("A comment must be between 2 and 250 characters long!")
-                            }}/>
-                            <button>Comment</button>
-                        </form>
-                    { data.comments.length > 0 && data.comments.map((comment) => {
-                        return (
-                            <div key={comment._id}>
-                                <h4 className='username clickable' onClick={() => {
-                                        navigate(`/user/${comment.author._id}`);
-                                }}>{comment.author.username}</h4>
-                                <div>{comment.date}</div>
-                                <p>{comment.text}</p>
-                            </div>
-                        )
-                    })}
-                    </section>
+                    <CommentForm data={data} formSubmission={formSubmission}></CommentForm>
                 </>
             }
             { errors !== null && 
