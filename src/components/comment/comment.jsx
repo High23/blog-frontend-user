5cae3a54b7f436ad0da3db6eff53b2de1d04281a
High23 <warenasb2@gmail.com>
2024-07-10 20:32:01 -0700

    Removed custom validity on textarea

--- src/components/comment/comment.jsx
+++ src/components/comment/comment.jsx
@@ -8,9 +8,7 @@ export default function CommentForm({data, formSubmission}) {
             <h2>Comments</h2>
             <form action="" method="post" onSubmit={(form) => {formSubmission(form)}}>
                 <label htmlFor="commentText"></label>
-                <textarea type="text" name="commentText" id="commentText" cols={40} rows={3} minLength={2} maxLength={250} required onInvalid={(e) => {
-                    e.target.setCustomValidity("A comment must be between 2 and 250 characters long!")
-                }}/>
+                <textarea type="text" name="commentText" id="commentText" cols={40} rows={3} minLength={2} maxLength={250} required/>
                 <button>Comment</button>
             </form>
         { data.postComments.length > 0 && data.postComments.map((comment) => {
